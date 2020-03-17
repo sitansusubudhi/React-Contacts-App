4d4e8234249d001e4168fcaeff6d79599c5e9a2d
Sitansu Subudhi <sitansu.subudhi@outlook.com>
2020-03-18 01:01:48 +0530

    Updates

---
+++ precache-manifest.36ac2449cfdfcd9b62c90be7ad0a1f06.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e96146bf58f0806c3b0f0190d5e5898",
+    "url": "/React-Contacts-App/static/media/search.8e96146b.svg"
+  },
+  {
+    "revision": "42e8dd55b3fc49244411ec110a355d0d",
+    "url": "/React-Contacts-App/static/media/person.42e8dd55.svg"
+  },
+  {
+    "revision": "d1d02572eb899e0fda0fb2962b529fc1",
+    "url": "/React-Contacts-App/static/media/person-add.d1d02572.svg"
+  },
+  {
+    "revision": "4fb2161211b0eea52425b59430e2fa88",
+    "url": "/React-Contacts-App/static/media/cancel.4fb21612.svg"
+  },
+  {
+    "revision": "7ee640f70ac495811737bcb55afa7ef5",
+    "url": "/React-Contacts-App/static/media/arrow-back.7ee640f7.svg"
+  },
+  {
+    "revision": "216e0c7dfa8286e27ceb",
+    "url": "/React-Contacts-App/static/js/runtime~main.216e0c7d.js"
+  },
+  {
+    "revision": "0df3762ad67d920e6788",
+    "url": "/React-Contacts-App/static/js/main.0df3762a.chunk.js"
+  },
+  {
+    "revision": "63a27a262cd99e2d527e",
+    "url": "/React-Contacts-App/static/js/1.63a27a26.chunk.js"
+  },
+  {
+    "revision": "0df3762ad67d920e6788",
+    "url": "/React-Contacts-App/static/css/main.ee438c8b.chunk.css"
+  },
+  {
+    "revision": "7bc1d4d0b1d5dd4f91a269cc590420cb",
+    "url": "/React-Contacts-App/index.html"
+  }
+];
