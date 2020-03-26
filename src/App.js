94f21f96dcb0fd6dfdb8b5b2f4e10ba093da1836
Sitansu Subudhi <sitansu.subudhi@outlook.com>
2020-03-27 00:19:52 +0530

    feat: Use React hooks to persist changes

--- src/App.js
+++ src/App.js
@@ -1,60 +1,73 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import ListContacts from './ListContacts'
 import * as ContactsAPI from './utils/ContactsAPI'
 import CreateContact from './CreateContact'
 import { Route } from 'react-router-dom'
 
-class App extends Component {
-  state = {
-    contacts: []
-  }
-  componentDidMount() {
-    // ContactsAPI.getAll()
-    ContactsAPI.getAllContacts()
-      .then((contacts) => {
-        this.setState(() => ({
-          contacts
-        }))
-      })
-  }
-  removeContact = (contact) => {
-    this.setState((currentState) => ({
-      contacts: currentState.contacts.filter((c) => {
-        return c.id !== contact.id
-      })
-    }))
+const LOCAL_STORAGE_KEY = "contactsApp.contacts"
+
+function App() {
+
+  const [contacts, setContacts] = useState([])
+
+  useEffect(() => {
+    // localStorage.clear()
+
+    const storedContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedContacts) {
+      setContacts(storedContacts)
+    } else {
+      ContactsAPI.getAllContacts()
+        .then((contacts) => {
+          localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts))
+          setContacts(contacts)
+        })
+    }
+
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts))
+  }, [contacts])
+
+
+  function removeContact(contact) {
+    const newContacts = contacts.filter((c) => {
+      return c.id !== contact.id
+    })
+
+    setContacts(newContacts)
 
     // ContactsAPI.remove(contact)
   }
-  createContact = (contact) => {
+  function createContact(contact) {
     ContactsAPI.create(contact)
       .then((contact) => {
-        console.log(contact);
-        this.setState((currentState) => ({
-          contacts: currentState.contacts.concat([contact])
-        }))
+        // console.log(contact);
+        setContacts(prevContacts =>
+          [...prevContacts, contact]
+        )
       })
   }
-  render() {
-    return (
-      <div>
-        <Route exact path={process.env.PUBLIC_URL + '/'} render={() => (
-          <ListContacts
-            contacts={this.state.contacts}
-            onDeleteContact={this.removeContact}
-          />
-        )} />
-        <Route path={process.env.PUBLIC_URL + '/create'} render={({ history }) => (
-          <CreateContact
-            onCreateContact={(contact) => {
-              this.createContact(contact)
-              history.push(process.env.PUBLIC_URL + '/')
-            }}
-          />
-        )} />
-      </div>
-    )
-  }
+
+  return (
+    <div>
+      <Route exact path={process.env.PUBLIC_URL + '/'} render={() => (
+        <ListContacts
+          contacts={contacts}
+          onDeleteContact={removeContact}
+        />
+      )} />
+      <Route path={process.env.PUBLIC_URL + '/create'} render={({ history }) => (
+        <CreateContact
+          onCreateContact={(contact) => {
+            createContact(contact)
+            history.push(process.env.PUBLIC_URL + '/')
+          }}
+        />
+      )} />
+    </div>
+  )
 }
 
 export default App
